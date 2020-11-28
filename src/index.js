9b716bad4d62596043ba09969c297396011639c0
Dmytro Rudakov <dimitry.rd@gmail.com>
2020-11-28 14:38:13 +0200

    Add input types for Comment User Post

--- src/index.js
+++ src/index.js
@@ -1,47 +1,76 @@
 import { GraphQLServer } from 'graphql-yoga';
+import uuidv4 from 'uuid/v4';
 
 // Scalar types - String, Int, Boolean, Float, ID
 
 // Demo user data
 const users = [
   {
-    id: 1,
+    id: '1',
     name: 'John',
     email: 'joe@example.com',
     age: 30,
   },
   {
-    id: 2,
+    id: '2',
     name: 'Jane',
     email: 'jane@example.com',
   },
   {
-    id: 3,
+    id: '3',
     name: 'George',
     email: 'george@example.com',
   },
 ];
+
 const posts = [
   {
-    id: 1,
+    id: '1',
     title: 'Hello',
-    body: 'Voluptate aliqua   anim eu do tempor laboris.',
+    body: 'Voluptate aliqua anim eu do tempor laboris.',
     published: true,
-    author: 1,
+    author: '1',
   },
   {
-    id: 2,
+    id: '2',
     title: 'Hello',
     body: 'Fugiat veniam reprehenderit nisi ad.',
     published: true,
-    author: 1,
+    author: '1',
   },
   {
-    id: 3,
+    id: '3',
     title: 'Hello',
     body: 'Laborum nulla do magna laborum quis dolore.',
     published: true,
-    author: 2,
+    author: '2',
+  },
+];
+
+const comments = [
+  {
+    id: '1',
+    text: 'qureto novellare, ancora potendo io infiniti quali raccontare piú nel noi. Quella ma non forza.',
+    author: '1',
+    post: '1',
+  },
+  {
+    id: '2',
+    text: 'Sed est aliquyam at invidunt et. Eos at sit tempor kasd labore voluptua est kasd',
+    author: '2',
+    post: '2',
+  },
+  {
+    id: '3',
+    text: 'Les peaux-rouges neiges et verte les. Golfes de heurté bleme verte si. Béni les.',
+    author: '1',
+    post: '2',
+  },
+  {
+    id: '4',
+    text: 'Ut diam dolore eirmod diam amet ipsum, et sit sed duo labore consetetur et sadipscing e',
+    author: '3',
+    post: '2',
   },
 ];
 
@@ -50,15 +79,42 @@ const typeDefs = `
     type Query {
         users(query: String): [User!]!
         posts(query: String): [Post!]!
+        comments: [Comment!]!
         me: User!
         post: Post!
     }
+    type Mutation {
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput): Post!
+        createComment(data: CreateCommentInput): Comment!
+    }
+
+    input CreateUserInput {
+      name: String!,
+      email: String!,
+      age: Int
+    }
+
+    input CreatePostInput {
+      title: String!,
+      body: String!,
+      published: Boolean,
+      author: ID!
+    }
+
+    input CreateCommentInput {
+      text: String!,
+      author: ID!,
+      post: ID!
+    }
+
     type User {
         id: ID!
         name: String!
         email: String!
         age: Int
         posts: [Post!]!
+        comments: [Comment!]!
     }
     type Post {
         id: ID!
@@ -66,6 +122,13 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comment: [Comment!]!
+    }
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
     }
 `;
 
@@ -73,6 +136,7 @@ const typeDefs = `
 const resolvers = {
   Query: {
     users(parent, args, context, info) {
+      console.log('🚀 ~ file: index.js ~ line 119 ~ users ~ args', args);
       if (!args.query) {
         return users;
       }
@@ -90,27 +154,11 @@ const resolvers = {
         return isBodyMatch || isTitleMatch;
       });
     },
-    // add(parent, args, context, info) {
-    //   return args.a + args.b;
-    // },
-    // addArray(parent, args, context, info) {
-    //   if (args.numbers.isArray) {
-    //   }
-    //   return args.numbers.reduce((i, s) => {
-    //     return i + s;
-    //   });
-    // },
-    // greetings(parent, args, context, info) {
-    //   console.log('greetings -> args', args);
-    //   console.log('greetings -> parent', parent);
-    //   console.log('greetings -> context', context);
-    //   console.log('greetings -> info', info);
-    //   if (args.a && args.b) {
-    //     return args.a + args.b;
-    //   } else {
-    //     return 'Please provide figures';
-    //   }
-    // },
+    comments(parent, args, context, info) {
+      if (!args.query) {
+        return comments;
+      }
+    },
     me() {
       return {
         id: '123123',
@@ -124,22 +172,88 @@ const resolvers = {
         title: 'First post',
         body: 'First post',
         published: true,
+        author: '1',
       };
+    },
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some((user) => user.email === args.data.email);
+
+      if (emailTaken) {
+        throw new Error('Email taken.');
+      }
+
+      const user = {
+        id: uuidv4(),
+        ...args.data,
+      };
+
+      users.push(user);
+
+      return user;
+    },
+    createPost(parent, args, context, info) {
+      const userExist = users.some((user) => user.id === args.data.author);
+
+      if (!userExist) {
+        throw new Error('User is not exist');
+      }
+
+      const post = {
+        id: uuidv4(),
+        ...args.data,
+      };
+
+      posts.push(post);
+
+      return post;
+    },
+    createComment(parent, args, context, info) {
+      const postExist = posts.some((post) => post.id === args.data.post && post.published);
+      const userExist = users.some((user) => user.id === args.data.author);
+
+      if (!postExist || !userExist) {
+        throw new Error('Unable to process the comment');
+      }
+
+      const comment = {
+        id: uuidv4(),
+        ...args.data,
+      };
+
+      comments.push(comment);
+
+      return comment;
     },
   },
   Post: {
     author(parent, args, context, info) {
-      console.log('author -> parent', parent);
-      return users.find((user) => {
-        return user.id === parent.author;
-      });
+      return users.find((user) => user.id === parent.author);
+    },
+    comment(parent, args, context, info) {
+      return comments.filter((comment) => comment.post === parent.id);
     },
   },
   User: {
     posts(parent, args, context, info) {
-      console.log('posts -> parent', parent);
+      return posts.filter((post) => post.author === parent.id);
+    },
+    comments(parent, args, context, info) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id;
+      });
+    },
+  },
+  Comment: {
+    author(parent, args, context, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+    post(parent, args, context, info) {
       return posts.find((post) => {
-        return (post.author = parent.author);
+        return post.id === parent.post;
       });
     },
   },
